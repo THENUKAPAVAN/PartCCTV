b01b2ac4e2db6dc5007c00c0c0f57f9875065493
Grigory E <miron36357@yandex.ru>
2016-06-28 00:45:09 +0300

    API

--- frontend/index.php
+++ frontend/index.php
@@ -4,9 +4,53 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application(); 
 
-$app->get('/web_gui/{name}', function($name) use($app) { 
-    return 'Hello GUI'.$app->escape($name); 
+$app->get('/web_gui/', function() { 
+
+	ob_start();
+	include 'webgui.phtml';
+	return (ob_get_clean());
+
 }); 
+
+$app->get('/api/1.0/platform/status', function (Silex\Application $app) {
+
+});
+
+$app->get('/api/1.0/platform/reload', function (Silex\Application $app) {
+
+});
+
+$app->get('/api/1.0/platform/restart', function (Silex\Application $app) {
+
+});
+
+$app->get('/api/1.0/camera/list', function (Silex\Application $app) {
+
+});
+
+$app->get('/api/1.0/camera/{camera}/settings', function (Silex\Application $app, $camera) {
+
+});
+
+$app->post('/api/1.0/camera/new', function (Silex\Application $app) {
+
+});
+
+$app->put('/api/1.0/camera/{camera}', function (Silex\Application $app, $camera) {
+
+});
+
+$app->delete('/api/1.0/camera/{camera}', function (Silex\Application $app, $camera) {
+
+});
+
+$app->get('/api/1.0/archive/list', function (Silex\Application $app) {
+
+});
+
+$app->get('//api/1.0/archive/camera/{camera}/list', function (Silex\Application $app, $camera) {
+
+});
 
 $app->get('/api/hello/{name}', function($name) use($app) { 
     return 'Hello '.$app->escape($name); 
